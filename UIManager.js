73ad90b9370f22e62967dfa20b75e26fd61f1b59
korinfigel <37318769+korinfigel@users.noreply.github.com>
2020-01-21 16:04:30 -0600

    Test

--- UIManager.js
+++ UIManager.js
@@ -163,7 +163,7 @@ export default class UIManager {
         this.formData.codename = e.target.value;
         this.formData.id = e.target.options[e.target.selectedIndex].id;
 
-        document.getElementById('image').src = `https://res.cloudinary.com/${this.options.acctName}/image/upload/${this.formData.codename}`;
+        document.getElementById('image').src  = `https://res.cloudinary.com/${this.options.acctName}/image/upload/${this.formData.codename}`;
     }
 
     updateValue(pictureStr) {
