008adb0d57246c26fdf2b3a9e349eb98699818e8
korinfigel <37318769+korinfigel@users.noreply.github.com>
2020-01-27 13:23:03 -0600

    Update UIManager.js

--- UIManager.js
+++ UIManager.js
@@ -254,9 +254,9 @@ export default class UIManager {
 
         for (var i = 1; i < assetSelector.options.length; i++) {
             for (var x = 0; x < assetSelector.options[i].tags.length; x++) {
-                if (assetSelector.options[i].tags[x] == e.target.value) {
+                if (assetSelector.options[i].tags[x].value == e.target.value) {
                     console.log("Match!");
-                    console.log(assetSelector.options[i].tags[x]);
+                    console.log(assetSelector.options[i]);
                 }
             }
         }
