f242c7f82c913e7ee416641bdda02fcbe7f38c0a
korinfigel <37318769+korinfigel@users.noreply.github.com>
2020-01-27 13:02:36 -0600

    Update UIManager.js

--- UIManager.js
+++ UIManager.js
@@ -244,7 +244,7 @@ export default class UIManager {
         //console.log(e);
 
         for (var i = 0; i < assetSelector.options.length; i++) {
-            if (assetSelector.options[i].tags.includes(e.target.value)) {
+            if (assetSelector.options[i].tags.indexOf(e.target.value)) {
                 console.log("item should be removed");
                 console.log(assetSelector.options[i]);
             }
